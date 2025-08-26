fddcd751174a7e0d78ffed269f8777dbb3229931
prefjordanpro <166382493+prefjordanpro@users.noreply.github.com>
2025-08-26 16:43:54 +0200

    espace membre deco co

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
+    }
+}
