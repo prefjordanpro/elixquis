ac66be2a8e5b83e4ed6b821be842708731bad349
prefjordanpro <166382493+prefjordanpro@users.noreply.github.com>
2025-08-28 14:23:26 +0200

    espace membre

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -31,6 +31,10 @@ final class AccountController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
+            $this->addFlash(
+                'success',
+                "Votre mot de passe est correctement mis à jour."
+            );
         }
 
         return $this->render('account/password.html.twig', [
