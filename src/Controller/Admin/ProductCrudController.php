5e7269702064c9d64c5474586afe4d6ef236b81e
prefjordanpro <166382493+prefjordanpro@users.noreply.github.com>
2025-08-29 16:54:52 +0200

    ajout des image dans les produits calcul tva

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Produit')
+            ->setEntityLabelInPlural('Produits')
+            ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')->setLabel('Nom')->setHelp('Nom de votre produit'),
+            SlugField::new('slug')->setTargetFieldName('name')->setLabel('URL')->setHelp('URL de votre catégorie générée automatiquement'),
+            TextEditorField::new('description')->setLabel('Description')->setHelp('Description de votre produit'),
+            ImageField::new('illustration')->setLabel('image')->setHelp('Image du produit')->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')->setBasePath('/uploads')->setUploadDir('/public/uploads'),
+            NumberField::new('price')->setLabel('Prix H.T')->setHelp('Prix H.T du produit sans le sigle €.'),
+            ChoiceField::new('tva')->setLabel('Taux de TVA')->setChoices([
+                '5.5%'=>'5.5',
+                '10%'=>'10',
+                '20%'=>'20'
+            ]),
+            AssociationField::new('category')->setLabel('Catégorie associée')
+        ];
+    }
+}
