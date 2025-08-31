87cf0b71695ab5af39fa7e808b591c907fa10f7f
prefjordanpro <166382493+prefjordanpro@users.noreply.github.com>
2025-08-31 11:38:28 +0200

    css ok

--- src/Form/AddressUserType.php
+++ src/Form/AddressUserType.php
@@ -2,28 +2,67 @@
 
 namespace App\Form;
 
-use App\Entity\Address;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Address;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+
 
 class AddressUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('address')
-            ->add('postal')
-            ->add('city')
-            ->add('country')
-            ->add('phone')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre nom'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Votre adresse',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre adresse'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Votre code postal',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre code postal'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Votre ville',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre ville'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Votre pays',
+                'preferred_choices' => ['FR']
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Votre téléphone',
+                'attr' => [
+                    'placeholder' => 'Indiquez votre numéro de téléphone'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Sauvegarder",
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
             ])
         ;
     }
