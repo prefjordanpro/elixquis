308ab0fb839656975e5d0bd6b94198f37a47d3b3
prefjordanpro <166382493+prefjordanpro@users.noreply.github.com>
2025-09-11 17:23:33 +0200

    ok

--- src/Controller/Account/HomeController.php
+++ src/Controller/Account/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Account;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,9 +11,20 @@ final class HomeController extends AbstractController
 {
 
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepository): Response
     {
-        return $this->render('account/index.html.twig');
+        $orders = $orderRepository->findBy([
+            'user' => $this->getUser(),
+            'state' => [1,2,3,4]
+        ],
+        [
+        'createdAt' => 'DESC',   // 👉 plus récent en premier
+        ]
+    );
+
+        return $this->render('account/index.html.twig', [
+            'orders' => $orders
+        ]);
     }
 
 }
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderRepository;
 use Stripe\Stripe;
+use App\Classe\Cart;
 use Stripe\Checkout\Session;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class PaymentController extends AbstractController
 {
     #[Route('/commande/paiement/{id_order}', name: 'app_payment')]
-    public function index($id_order, OrderRepository $orderRepository): Response
+    public function index($id_order, OrderRepository $orderRepository, EntityManagerInterface $entityManager): Response
     {   
         $stripe = new \Stripe\StripeClient($_ENV['STRIPE_SECRET_KEY']);
         $YOUR_DOMAIN = 'http://127.0.0.1:8000';
@@ -62,12 +64,42 @@ final class PaymentController extends AbstractController
             'customer_email' => $this->getUser()->getEmail(),
             'line_items' => $products_for_stripe, // ✅ on passe directement le tableau
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url'  => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/commande/merci/{CHECKOUT_SESSION_ID}',
+            'cancel_url'  => $YOUR_DOMAIN . '/mon-panier/annulation',
             'automatic_tax' => ['enabled' => false],
         ]);
 
+
+        $order->setStripeSessionId($checkout_session->id);
+        $entityManager->flush();
+
+
+
         return $this->redirect($checkout_session->url);
+    }
+
+    #[Route('/commande/merci/{stripe_session_id}', name: 'app_payment_success')]
+    public function success($stripe_session_id, OrderRepository $orderRepository, EntityManagerInterface $entityManager, Cart $cart): Response
+    {
+            $order = $orderRepository->findOneBy([
+            'stripe_session_id' => $stripe_session_id,
+            'user' => $this->getUser()
+        ]);
+
+        if (!$order) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        if ($order->getState() == 0) {
+            $order->setState(1);
+            $cart->remove();
+            $entityManager->flush();
+        }
+
+        return $this->render('payment/success.html.twig', [
+            'order' => $order,
+        ]);
+
     }
 }
 
